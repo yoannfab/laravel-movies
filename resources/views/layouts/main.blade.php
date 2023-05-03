4d074881b95df26731c1820a386069ec2573657d
Yoann <yoann.mukendi@gmail.com>
2023-05-03 06:48:43 +0200

    fist commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Movie App</title>
+
+    <link rel="stylesheet" href="/css/main.css">
+    <livewire:styles>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+</head>
+<body class="font-sans bg-gray-900 text-white">
+    <nav class="border-b border-gray-800">
+        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-row items-center">
+                <li class="md:ml-16 mt-3 md:mt-0">
+                    <a href="{{ route('movies.index') }}" class="hover:text-gray-300">Movies</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="{{ route('tv.index') }}" class="hover:text-gray-300">TV Shows</a>
+                </li>
+                <li class="md:ml-6 mt-3 md:mt-0">
+                    <a href="{{ route('actors.index') }}" class="hover:text-gray-300">Actors</a>
+                </li>
+            </ul>
+            <div class="flex flex-col md:flex-row items-center">
+                <livewire:search-dropdown>
+            </div>
+        </div>
+    </nav>
+    @yield('content')
+    <footer class="border border-t border-gray-800">
+        <div class="container mx-auto text-sm px-4 py-6">
+            Powered by <a href="https://www.themoviedb.org/documentation/api" class="underline hover:text-gray-300">TMDb API</a>
+        </div>
+    </footer>
+    <livewire:scripts>
+    @yield('scripts')
+</body>
+</html>
